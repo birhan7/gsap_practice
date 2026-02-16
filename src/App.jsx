9a9174477cbe39e5053296d05cd077ab7d2dd701
Birhan Aklilu <birhanaklilu102@gmail.com>
2026-02-17 01:24:44 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex-center h-[100vh]">
+      <p className="text-3xl text-indigo-900">App</p>
+    </div>
+  );
+}
+
+export default App;
